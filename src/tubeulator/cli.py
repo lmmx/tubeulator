ae74bbe9808e366650dfd1387f960852f5969b46
Louis Maddox <louismmx@gmail.com>
2022-06-12 17:55:57 +0100

    Switch to defopt for namespace inventory approach

--- src/tubeulator/cli.py
+++ src/tubeulator/cli.py
@@ -1,3 +1,5 @@
+import defopt
+
 from pymongo import MongoClient
 
 from .db.store_creds import check_creds
@@ -5,8 +7,24 @@ from .db.mongod import MongodExceptionGuard
 from .api.line import line_data
 
 
-def main():
+def lines():
     with MongodExceptionGuard():
         client = MongoClient()
         creds = check_creds(client=client, interactive=True)
         line_data(creds)
+
+def namespace():
+    """
+    Make a namespace inventory
+    """
+    ... # TODO
+
+def main():
+    defopt.run({
+        "names": namespace,
+        "lines": lines,
+    },
+        no_negated_flags=True,
+        strict_kwonly=False,
+        short={},
+    )
