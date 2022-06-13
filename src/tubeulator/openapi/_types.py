d33d855a59e89e06ca194791664cd6b4c1ab625d
Louis Maddox <louismmx@gmail.com>
2022-06-13 13:10:40 +0100

    Refactor API schema into dataclasses

--- src/tubeulator/openapi/_types.py
+++ src/tubeulator/openapi/_types.py
@@ -1,13 +1,13 @@
 from __future__ import annotations
 
-__all__ = ["ApiInventory", "NamespaceInventory"]
+__all__ = ["ApiName", "ApiEntityAlias", "UnifiedApiEntity", "ApiAliasToUnifiedEntities", "NamespaceInventory"]
 
 ApiName = str
 """
 The name of an API (which is also the stem of the API schema file itself).
 """
 
-class ApiEntity(str):
+class ApiEntityAlias(str):
     """
     An API entity (typically an alias of the form "Tfl", "Tfl-1", "Tfl-2", etc.) from a
     non-unified API.
@@ -18,7 +18,7 @@ class UnifiedApiEntity(str):
     An API entity from the unified API
     """
 
-ApiInventory = dict[ApiEntity, list[UnifiedApiEntity]]
+ApiAliasToUnifiedEntities = dict[ApiEntityAlias, list[UnifiedApiEntity]]
 """
 An inventory of API entity aliases and the properly named entities they correspond to
 from the unified API. Since there is no guarantee that different unified API entities
@@ -26,7 +26,7 @@ cannot have identical schemas, there may be a one-to-many mapping (i.e. the dict
 list-valued).
 """
 
-NamespaceInventory = dict[ApiName, ApiInventory]
+NamespaceInventory = dict[ApiName, ApiAliasToUnifiedEntities]
 """
 The namespace of all APIs and their inventories mapping each of their API entities
 (aliases) to the properly named entities they correspond to from the unified API.
--- src/tubeulator/openapi/scan.py
+++ src/tubeulator/openapi/scan.py
@@ -1,76 +1,166 @@
 from __future__ import annotations
 
-from ._types import ApiInventory, NamespaceInventory
-from ..utils.paths import openapi_schemas_path, unified_api_schema, find_schema
+from ._types import ApiAliasToUnifiedEntities, NamespaceInventory
+from ..utils.paths import openapi_schemas_path, unified_api_schema as unified_api_schema_path, find_schema
 
 import ujson
+from dataclasses import dataclass
 from pathlib import Path
 from pprint import pprint
+from typing import Literal
 
 __all__ = ["api_schema_inventory", "scan_namespace"]
 
+@dataclass
+class Reference:
+    schema: str
+    alias: str
+    referential_property: str
+    # path: tuple[Literal["items","$ref"]]
+    is_array: bool
+    entity: str
+
+    @property
+    def path(self) -> tuple[Literal["items","$ref"]]:
+        return tuple([*(["items"] if self.is_array else []), "$ref"])
+
+    @property
+    def substituted_ref(self) -> str:
+        return Path(self.entity).name
+
+    def is_completable(self, api_inventory: ApiAliasToUnifiedEntities):
+        """
+        If the API inventory has a resolved value (or values) for the alias referenced
+        in the current referential property's ``$ref`` key, then the reference can be
+        resolved immediately (i.e. "completed"). If not, we have to wait for the next
+        round of substitutions, or chase references.
+        """
+        if self.substituted_ref in api_inventory:
+            return api_inventory[self.substituted_ref] != []
+        else:
+            raise ValueError(f"{self.substituted_ref=} not found in {api_inventory=}")
+
+@dataclass
+class ApiSchema:
+    path: Path
+    unified: bool
+
+    def __post_init__(self):
+        if self.path.suffix != ".json":
+            raise ValueError(f"{self.path=} is not a JSON file")
+        self.obj = ujson.loads(self.path.read_text())
+        # Discard any of the entities with 'Array' in, they're not useful
+        self.entity_schemas = {
+            name: schema
+            for name, schema in self.obj["components"].get("schemas", {}).items()
+            if "Array" not in name
+        }
+    
+    @property
+    def name(self) -> str:
+        return self.path.stem
+
+class UnifiedApiSchema(ApiSchema):
+    """
+    A unified API schema.
+    """
+    unified: bool = True
+
+single_unified_api_schema = UnifiedApiSchema(path=unified_api_schema_path, unified=True)
+"""
+A singleton instance to avoid re-reading the unified API schema JSON once per
+non-unified API schema instantiation.
+"""
+
+class AliasedApiSchema(ApiSchema):
+    """
+    A non-unified API schema.
+    """
+    unified: bool = False
+
+    def __post_init__(self):
+        super().__post_init__()
+        self.unified_schema = single_unified_api_schema
+        self.alias2ents: ApiAliasToUnifiedEntities = {
+            entity_alias: []
+            for entity_alias in self.entity_schemas # (i.e. its keys)
+        }
+        self.match_entities()
+        self.resolve_entity_references()
+
+    def match_entities(self):
+        """
+        First pass, get the exact matches. Iterate over the aliases (the entity names in
+        the API schema "broken out" from the unified schema) and where the schema
+        component is an exact match for one in the unified schema, store the entity name
+        (or names: there is no guarantee only a single entity will match the alias).
+        """
+        for entity_alias, schema_component in self.entity_schemas.items():
+            for entity, unif_schema in self.unified_schema.entity_schemas.items():
+                if schema_component == unif_schema:
+                    self.alias2ents[entity_alias].append(entity)
+
+    def resolve_entity_references(self):
+        """
+        Second pass, resolve the referential matches with substitution of known values.
+        Again iterate over the aliases, but now try to find where the schema component
+        can be substituted if the reference is 'completable' to consequently make an
+        exact match for one of the schema components in the unified schema. As for the
+        previous pass, store the entity name or names alongside the alias if successful.
+        """
+        self.any_referential_properties = False
+        for entity_alias, schema_component in self.entity_schemas.items():
+            if (asc_properties := schema_component.get("properties")):
+                referential_properties = {
+                    asc_property: {
+                        k: v
+                        for k,v in asc_property_type_info.items()
+                        if k in ["$ref", "items"]
+                    }
+                    for asc_property, asc_property_type_info in asc_properties.items()
+                    if (
+                        "$ref" in asc_property_type_info
+                        or "$ref" in asc_property_type_info.get("items", {})
+                    )
+                }
+                # Either a top-level $ref or as the items in an array.
+                # (Valid) arrays have items with a specified $ref
+                asc_property_refs = [
+                    # Use next as can rely on only 1 reference per property
+                    next(
+                        Reference(
+                            schema=self.name,
+                            alias=entity_alias,
+                            referential_property=asc_prop,
+                            # path=tuple([*(["items"] if (is_nested_ref := isinstance(v, dict)) else []), "$ref"]),
+                            is_array=isinstance(v, dict),
+                            entity=v["$ref"] if (is_nested_ref := isinstance(v, dict)) else v,
+                        )
+                        for v in asc_prop_type_info.values()
+                    )
+                    for asc_prop, asc_prop_type_info in referential_properties.items()
+                ]
+                if referential_properties:
+                    pprint(asc_property_refs)
+                    self.any_referential_properties = True # toggle flag for follow-up
+                    completability = [
+                        asc_property_ref.is_completable(api_inventory=self.alias2ents)
+                        for asc_property_ref in asc_property_refs
+                    ]
+                    print(f"Completable: {completability.count(True)}, incomplete: {completability.count(False)}")
+        # Print the API inventory and a newline if any referential properties printed
+        if self.any_referential_properties:
+            pprint({k: v for k,v in self.alias2ents.items() if "Response" not in k})
+            print()
 
 # TODO: duplication here suggests need for an API schema class (from JSON to entities)
-def api_schema_inventory(api_schema: Path) -> ApiInventory:
+def api_schema_inventory(api_schema: Path) -> ApiAliasToUnifiedEntities:
     """
     Make a mapping of all aliases (the arbitrarily named entities) to their (properly
     named) unified entity names.
     """
-    if api_schema.suffix != ".json":
-        raise ValueError(f"{api_schema=} is not a JSON file")
-    api_schema_obj = ujson.loads(api_schema.read_text())
-    api_entity_schemas = api_schema_obj["components"].get("schemas", {})
-    # Drop any of the entities with 'Array' in, they're not useful
-    api_entity_schemas = {
-        name: schema
-        for name, schema in api_entity_schemas.items()
-        if "Array" not in name
-    }
-    unified_api_schema_obj = ujson.loads(unified_api_schema.read_text())
-    unified_api_entity_schemas = unified_api_schema_obj["components"]["schemas"]
-    alias2entity: ApiInventory = {
-        api_entity: []
-        for api_entity in api_entity_schemas
-    }
-    # First pass, get the exact matches
-    for api_entity, api_schema_component in api_entity_schemas.items():
-        # if api_schema.stem == "occupancy" and api_entity == "Tfl-2":
-        #     breakpoint()
-        for unified_entity, unified_schema in unified_api_entity_schemas.items():
-            if api_schema_component == unified_schema:
-                alias2entity[api_entity].append(unified_entity)
-    # TODO: second pass, with substitution of known values
-    # TODO: make a class to help identify which aliases are in each schema,
-    #       which would then be easier to indicate those 'completable' by substitutions
-    # Second pass, resolve the referential matches
-    any_referential_properties = False
-    for api_entity, api_schema_component in api_entity_schemas.items():
-        if (asc_properties := api_schema_component.get("properties")):
-            referential_properties = {
-                asc_property: {
-                    k: v
-                    for k,v in asc_property_type_info.items()
-                    if k in ["$ref", "items"]
-                }
-                for asc_property, asc_property_type_info in asc_properties.items()
-                if (
-                    "$ref" in asc_property_type_info
-                    or "$ref" in asc_property_type_info.get("items", {})
-                )
-            }
-            if referential_properties:
-                print(f"{api_schema.stem}: {api_entity}")
-                pprint(referential_properties)
-                any_referential_properties = True # toggle flag for follow-up
-    # Print the API inventory and a newline if any referential properties printed
-    if any_referential_properties:
-        pprint({k: v for k,v in alias2entity.items() if "Response" not in k})
-        print()
-    # {
-    #     api_entity: unified_api_entity
-    #     for api_entity in api_schema
-    # } # ApiInventory
-    return alias2entity
+    api_schema = AliasedApiSchema(path=api_schema, unified=False)
+    return api_schema.alias2ents
 
 
 def scan_namespace() -> NamespaceInventory:
@@ -81,5 +171,4 @@ def scan_namespace() -> NamespaceInventory:
     namespace_inventory = {
         api_schema.stem: api_schema_inventory(api_schema) for api_schema in api_schemas
     }
-    # ApiInventory = dict[ApiEntity, UnifiedApiEntity]
     return namespace_inventory
