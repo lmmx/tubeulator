353e620f96ffb4db2f3cff04f1f06783d02fa4ab
Louis Maddox <louismmx@gmail.com>
2023-03-21 00:03:29 +0000

    Complete initial [pseudo functional] API surface

---
+++ src/tubeulator/api/interface.py
@@ -0,0 +1,24 @@
+from .endpoint.names import EndpointNames
+from .endpoint.routes import EndpointRoute
+from .endpoint.routes.types import AnyEndpointRouteEnum
+from .request import Path, Request
+
+__all__ = ["EndpointInfo", "AllEndpointInterface", "get_info"]
+
+
+class EndpointInfo:
+    def __init__(self, endpoint: AnyEndpointRouteEnum):
+        for route in endpoint.value:
+            endpoint_name = EndpointNames[endpoint.name]
+            request = Request(path=Path(endpoint=endpoint_name, route=route))
+            setattr(self, route.name.lower(), request)
+
+
+class AllEndpointInterface:
+    def __init__(self):
+        for endpoint in EndpointRoute:
+            interface = EndpointInfo(endpoint=endpoint)
+            setattr(self, endpoint.name.lower(), interface)
+
+
+get_info = AllEndpointInterface()
