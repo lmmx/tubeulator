4fbc74e621085e43733e2820ec0213f87408e89a
Louis Maddox <louismmx@gmail.com>
2023-05-29 14:06:38 +0100

    Simple way to confirm generated modules are loadable

---
+++ src/tubeulator/codegen/load_test.py
@@ -0,0 +1,20 @@
+"""
+Import this module to check if the generated Python DTOs are loadable.
+"""
+
+from ..generated import (
+    AccidentStats,
+    AirQuality,
+    BikePoint,
+    DisruptionsLiftsv2,
+    Journey,
+    Line,
+    Mode,
+    Place,
+    Road,
+    Search,
+    StopPoint,
+    Vehicle,
+    crowding,
+    occupancy,
+)
