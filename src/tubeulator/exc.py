28cd0f365f63f7156d23a092a62200bde969f56d
Louis Maddox <louismmx@gmail.com>
2023-06-02 19:00:40 +0100

    Include route enum name, will make errors reproducible

--- src/tubeulator/exc.py
+++ src/tubeulator/exc.py
@@ -7,7 +7,7 @@ __all__ = ["RequestError"]
 
 class RequestError(Exception):
     def __init__(self, response: Response, path: Path, *args, **kwargs):
-        path_repr = f"{path.endpoint.name}:{path.route.value}"
+        path_repr = f"{path.endpoint.name}.{path.route.name}:{path.route.value}"
         reason = f"{response.status_code} {response.reason_phrase}"
         message = f"HTTP error at {path_repr} ({list(args)}, {kwargs})\n  {reason}"
         try:
