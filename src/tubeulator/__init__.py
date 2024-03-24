12c4ad23389fefddde851d8831b6581bc70646e5
Louis Maddox <louismmx@gmail.com>
2024-03-24 13:01:45 +0000

    chore: move to pyproject.toml (drop scm versioning)

--- src/tubeulator/__init__.py
+++ src/tubeulator/__init__.py
@@ -5,11 +5,6 @@ r"""
 
 __all__ = ["Journey"]
 
-__author__ = "Louis Maddox"
-__license__ = "MIT"
-__description__ = "TfL open data interface library."
-__url__ = "https://github.com/lmmx/tubeulator"
-__uri__ = __url__
-__email__ = "louismmx@gmail.com"
-
 from .api import fetch
+
+__version__ = "0.0.2"
