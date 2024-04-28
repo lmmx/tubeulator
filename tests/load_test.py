b213427b93e8870f622f2fa776ee4ad4bf26fa50
Louis Maddox <louismmx@gmail.com>
2024-04-28 12:53:58 +0100

    test: renames and tests for the combined data (effectively tests all topology loaders)

--- tests/load_test.py
+++ tests/load_test.py
@@ -1,6 +1,22 @@
-from tubeulator.network.load import load_transport_network
+import polars as pl
+from tubeulator.topology import load_lines_by_station, load_stations_by_line
+
+__all__ = ["test_load_stations_by_line", "test_load_lines_by_station"]
 
 
-def test_load_gtfs():
-    """This was a problem case due to the alias generator."""
-    load_transport_network()
+def test_load_stations_by_line():
+    line2stns = load_stations_by_line()
+    stn_counts = line2stns.with_columns(
+        pl.col("StationName").list.len().alias("Count")
+    ).sort("Count")
+    assert stn_counts["Count"].min() == 2
+    assert stn_counts["Count"].max() == 112
+
+
+def test_load_lines_by_station():
+    stn2lines = load_lines_by_station()
+    line_counts = stn2lines.with_columns(
+        pl.col("Lines").list.len().alias("Count")
+    ).sort("Count")
+    assert line_counts["Count"].min() == 1
+    assert line_counts["Count"].max() == 7
