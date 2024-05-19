7339f1e36c669a5ac8083f834c03cfdad37cb0bf
Louis Maddox <louismmx@gmail.com>
2024-05-20 00:35:52 +0100

    test: endpoint coverage progress test

--- tests/apis/line_data.py
+++ tests/apis/line_data.py
@@ -1,5 +1,33 @@
 from inline_snapshot import snapshot
 
+all_endpoints = snapshot(
+    [
+        "forward_requests",
+        "meta_disruption_categories",
+        "meta_modes",
+        "meta_service_types",
+        "meta_severity",
+        "lines_by_modes",
+        "disruption_by_modes",
+        "route_by_modes",
+        "status_by_modes",
+        "all_routes",
+        "search_lines_routes",
+        "status_by_severity",
+        "lines_by_ids",
+        "arrivals_by_ids",
+        "arrivals_by_ids_stop",
+        "disruption_by_ids",
+        "route_by_ids",
+        "status_by_ids",
+        "status_by_ids_dates",
+        "route_sequence_by_id_direction",
+        "stop_points_by_id",
+        "timetable_by_id_from_stop",
+        "timetable_by_id_from_to_stop",
+    ]
+)
+
 meta_modes_snapshot = snapshot(
     [
         {
@@ -338,7 +366,7 @@ line_id_album = {
     "waterloo-city": wc_line_snapshot,
 }
 
-line_snapshot_album = {
+snapshot_album = {
     "meta_modes": meta_modes_snapshot,
     "lines_by_ids": line_id_album,
     "lines_by_modes": tube_line_snapshot,
