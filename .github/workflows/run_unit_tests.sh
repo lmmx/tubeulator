624d1d9e559316038c0943247a2db6c68261d60f
Louis Maddox <lmmx@users.noreply.github.com>
2024-05-19 14:46:06 +0100

    ci tests (#53)
    * build: add inline-snapshot to test deps
    
    * test: CI test workflow with secrets and test matrix
    
    * refactor: delete unused line
    
    * chore(pre-commit): autofix run
    
    * refactor: misnamed extension
    
    * refactor: rename CI
    
    * build: limit CI test runs to paths which would potentially affect tests
    
    * feat: pick up credential from env var
    
    * feat: pick up env var credentials and handle as for MongoDB credentials
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ .github/workflows/run_unit_tests.sh
@@ -0,0 +1,84 @@
+name: Build and Test
+
+on:
+  push:
+    branches: [master]
+    paths:
+      - src/**
+      - tests/**
+      - pyproject.toml
+      - .github/workflows/**
+  pull_request:
+    branches: [master]
+    paths:
+      - src/**
+      - tests/**
+      - pyproject.toml
+      - .github/workflows/**
+
+jobs:
+  test-setup:
+    runs-on: ubuntu-latest
+    outputs:
+      test-matrix: ${{ steps.set-matrix.outputs.test_matrix }}
+    steps:
+      - uses: actions/checkout@v4
+
+      - id: set-matrix
+        run: |
+          ALL_TEST_TARGETS=$(find tests -mindepth 1 -type d -name *_test.py -exec realpath --relative-to=tests {} \;)
+          PACKAGE_MATRIX_JSON=$(echo "$ALL_TEST_TARGETS" | sort | jq -R -s -c 'split("\n")[:-1] | map({testtarget: .})')
+          echo "Setting test_matrix to: $PACKAGE_MATRIX_JSON"
+          echo "test_matrix=$PACKAGE_MATRIX_JSON" >> $GITHUB_OUTPUT
+
+  test:
+    runs-on: ubuntu-latest
+    needs: test-setup
+    env:
+      TFL_API_KEY_ID: ${{ secrets.TFL_API_KEY_ID }}
+      TFL_API_PRIMARY_ACCESS_KEY: ${{ secrets.TFL_API_PRIMARY_ACCESS_KEY }}
+      TFL_API_SECONDARY_ACCESS_KEY: ${{ secrets.TFL_API_SECONDARY_ACCESS_KEY }}
+    strategy:
+      fail-fast: false
+      matrix:
+        include: ${{ fromJson(needs.test-setup.outputs.test-matrix) }}
+    steps:
+      - uses: actions/checkout@v4
+
+      - name: Set up Python
+        uses: actions/setup-python@v5
+        with:
+          python-version: '3.12'
+
+      - name: Install PDM
+        run: |
+          python -m pip install --user pdm
+          pdm config install.cache on
+
+      - name: Install package dependencies and run unit tests
+        timeout-minutes: 5
+        run: |
+          set -ex
+          test_target=tests/${{ matrix.testtarget }}
+
+          INSTALL_START_TIME=$(date +%s)
+          echo "- Installing tubeulator :: $(date)"
+          if [[ ! -f "pyproject.toml" ]]; then
+            echo "pyproject.toml does not exist"
+            exit 1
+          fi
+          # This would become useful if .venv caching was introduced
+          if [[ ! -d ".venv" ]]; then
+            echo "Creating new venv from the test group of 'pyproject.toml'"
+            pdm install -dG test
+          fi
+          INSTALL_END_TIME=$(date +%s)
+          INSTALL_ELAPSED_TIME=$(($INSTALL_END_TIME - $INSTALL_START_TIME))
+          echo "- Finished installing $service, now running tests :: $(date)"
+
+          TEST_START_TIME=$(date +%s)
+          pdm run python -m pytest $test_target -m "not skiponci" -n auto -qq --disable-warnings --suppress-no-test-exit-code
+          TEST_END_TIME=$(date +%s)
+
+          TEST_ELAPSED_TIME=$(($TEST_END_TIME - $TEST_START_TIME))
+          echo "- Installed tubeulator (target: <code>$test_target</code>) in $INSTALL_ELAPSED_TIME seconds, tests passed in $TEST_ELAPSED_TIME seconds," >> $GITHUB_STEP_SUMMARY
--- src/tubeulator/db/store_creds.py
+++ src/tubeulator/db/store_creds.py
@@ -1,8 +1,16 @@
+import os
 from functools import lru_cache
 
+from pydantic import BaseModel, Field, ValidationError
 from pymongo import MongoClient
 
 from .mongod import MongodExceptionGuard
+
+
+class EnvCredentials(BaseModel):
+    app_id: str = Field(validation_alias="TFL_API_KEY_ID")
+    primary_key: str = Field(validation_alias="TFL_API_PRIMARY_ACCESS_KEY")
+    secondary_key: str = Field(validation_alias="TFL_API_SECONDARY_ACCESS_KEY")
 
 
 @lru_cache
@@ -10,28 +18,35 @@ def check_creds() -> dict[str, str]:
     """Store the API credentials in a simple singleton collection "tfl_cred" in the "creds"
     database if none has been stored there before. Interactive only.
     """
-    with MongodExceptionGuard():
-        client = MongoClient()
-        creds_collection = client.creds.tfl_cred
-        n_docs = creds_collection.estimated_document_count()
-        if n_docs == 0:
-            default_app_id = "tubeulator"
-            app_id = (
-                input(f"Enter your app ID (default: {default_app_id!r}): ")
-                or default_app_id
-            )
-            primary_key = input("Enter your primary key: ")
-            secondary_key = input("Enter your secondary key: ")
-            stored_credential = {
-                "app_id": app_id,
-                "primary_key": primary_key,
-                "secondary_key": secondary_key,
-            }
-            creds_collection.insert_one(stored_credential)
-        elif n_docs > 1:
-            raise ValueError(
-                f"Multiple credentials {n_docs=} found: database has been corrupted",
-            )
-        else:
-            stored_credential = creds_collection.find_one()
-        return stored_credential
+    try:
+        env_creds = EnvCredentials.parse_obj(os.environ)
+        credential = env_creds.model_dump()
+    except ValidationError:
+        # No API keys in environment variables
+        pass
+    else:
+        with MongodExceptionGuard():
+            client = MongoClient()
+            creds_collection = client.creds.tfl_cred
+            n_docs = creds_collection.estimated_document_count()
+            if n_docs == 0:
+                default_app_id = "tubeulator"
+                app_id = (
+                    input(f"Enter your app ID (default: {default_app_id!r}): ")
+                    or default_app_id
+                )
+                primary_key = input("Enter your primary key: ")
+                secondary_key = input("Enter your secondary key: ")
+                credential = dict(
+                    app_id=app_id,
+                    primary_key=primary_key,
+                    secondary_key=secondary_key,
+                )
+                creds_collection.insert_one(credential)
+            elif n_docs > 1:
+                raise ValueError(
+                    f"Multiple credentials {n_docs=} found: database has been corrupted",
+                )
+            else:
+                credential = creds_collection.find_one()
+    return credential
