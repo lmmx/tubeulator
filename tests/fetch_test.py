e38bcafa83e906a58faea508223f871909a76307
Louis Maddox <lmmx@users.noreply.github.com>
2024-05-19 18:07:27 +0100

    fix: CI repo variable usage (#57)
    * fix: comment out env var?
    
    * fix: bug in find command meant test matrix was empty; ensure always to clobber directories as regular files
    
    * test: remove skiponci test from fetch test (check CI can use API)
    
    * fix: repository variables go in the `vars` context

--- tests/fetch_test.py
+++ tests/fetch_test.py
@@ -1,8 +1,6 @@
 import tubeulator
-from pytest import mark
 
 
-@mark.skiponci
 def test_first_mode_is_bus():
     modes = tubeulator.fetch.line.meta_modes()
     first_mode_name = modes[0].ModeName
