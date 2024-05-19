8427d2a9a0bb6f08df07b31535a3d11fb38d19f0
Louis Maddox <lmmx@users.noreply.github.com>
2024-05-20 00:36:42 +0100

    lmmx/snapshot endpoint coverage (#68)
    * test: endpoint coverage progress test
    
    * style: lint

--- tests/apis/line_test.py
+++ tests/apis/line_test.py
@@ -1,8 +1,41 @@
 """Tests for the line API."""
 
 import tubeulator
-from line_data import line_snapshot_album as snapshot_album
+from line_data import all_endpoints, snapshot_album
 from pytest import mark
+
+tested_eps = {
+    "lines_by_ids",
+    "lines_by_modes",
+    "meta_modes",
+}
+untested_eps = {
+    "all_routes",
+    "arrivals_by_ids",
+    "arrivals_by_ids_stop",
+    "disruption_by_ids",
+    "disruption_by_modes",
+    "forward_requests",
+    "meta_disruption_categories",
+    "meta_service_types",
+    "meta_severity",
+    "route_by_ids",
+    "route_by_modes",
+    "route_sequence_by_id_direction",
+    "search_lines_routes",
+    "status_by_ids",
+    "status_by_ids_dates",
+    "status_by_modes",
+    "status_by_severity",
+    "stop_points_by_id",
+    "timetable_by_id_from_stop",
+    "timetable_by_id_from_to_stop",
+}
+
+
+def test_line_endpoints():
+    assert list(vars(tubeulator.fetch.line)) == all_endpoints
+    assert set(vars(tubeulator.fetch.line)) == tested_eps.union(untested_eps)
 
 
 @mark.parametrize("expected", [snapshot_album["meta_modes"]])
