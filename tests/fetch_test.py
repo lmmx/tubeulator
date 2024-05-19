ffb253acc362e414f6065307b34417e5c002d9e1
Louis Maddox <louismmx@gmail.com>
2024-05-19 18:39:01 +0100

    fix: snapshot the test

--- tests/fetch_test.py
+++ tests/fetch_test.py
@@ -1,4 +1,8 @@
+import datetime
+
 import tubeulator
+from inline_snapshot import snapshot
+from pydantic_core import TzInfo
 
 
 def test_first_mode_is_bus():
@@ -8,5 +12,33 @@ def test_first_mode_is_bus():
 
 
 def test_lines_by_ids():
-    """This failed due to a codegen error."""
+    """Relies on correct codegen of arrays (previously had a bug).
+
+    Note: the `TzInfo(0)` snapshots as `TzInfo(UTC)` for unclear reasons.
+    """
     lines = tubeulator.fetch.line.lines_by_ids("waterloo-city")
+    assert [l.model_dump() for l in lines] == snapshot(
+        [
+            {
+                "Id": "waterloo-city",
+                "Name": "Waterloo & City",
+                "ModeName": "tube",
+                "Disruptions": [],
+                "Created": datetime.datetime(
+                    2024, 5, 14, 14, 35, 6, 617000, tzinfo=TzInfo(0)
+                ),
+                "Modified": datetime.datetime(
+                    2024, 5, 14, 14, 35, 6, 617000, tzinfo=TzInfo(0)
+                ),
+                "LineStatuses": [],
+                "RouteSections": [],
+                "ServiceTypes": [
+                    {
+                        "Name": "Regular",
+                        "Uri": "/Line/Route?ids=Waterloo & City&serviceTypes=Regular",
+                    }
+                ],
+                "Crowding": {"PassengerFlows": [], "TrainLoadings": []},
+            }
+        ]
+    )
