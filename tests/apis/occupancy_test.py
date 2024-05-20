dc8571159b7e65ad5b539f60b7874ec56f00befb
Louis Maddox <louismmx@gmail.com>
2024-05-20 20:01:17 +0100

    test: occupancy API coverage

--- tests/apis/occupancy_test.py
+++ tests/apis/occupancy_test.py
@@ -1,18 +1,52 @@
 """Tests for the occupancy API."""
 
 from occupancy_data import all_endpoints
+from pytest import mark
 from tubeulator import fetch
 
-tested_eps = set()
-untested_eps = {
+tested_eps = {
     "bike_points",
-    "car_park",
-    "car_park_id",
     "charge_connector",
     "charge_connector_ids",
+}
+untested_eps = {
+    "car_park",
+    "car_park_id",
 }
 
 
 def test_occupancy_endpoints():
     assert list(vars(fetch.occupancy)) == all_endpoints
     assert set(vars(fetch.occupancy)) == tested_eps.union(untested_eps)
+
+
+@mark.skip(reason="KeyError: '$ref' in response_refpath")
+def test_bike_points():
+    bike_points = fetch.occupancy.bike_points(ids="BikePoints_100")
+    assert bike_points
+
+
+@mark.skip(reason="500: Internal server error")
+def test_car_park():
+    car_park = fetch.occupancy.car_park()
+    assert car_park
+
+
+@mark.skip(reason="500: Internal server error")
+def test_car_park_id():
+    car_park_id = fetch.occupancy.car_park_id(id="CarParks_800503")
+    assert car_park_id
+
+
+@mark.skip(reason="KeyError: '$ref' in response_refpath")
+def test_charge_connector():
+    charge_connector = fetch.occupancy.charge_connector()
+    assert charge_connector
+
+
+@mark.skip(reason="KeyError: '$ref' in response_refpath")
+def test_charge_connector_ids():
+    charge_connector_ids = fetch.occupancy.charge_connector_ids(
+        ids="ChargePointCM-BPT62200886-143260",
+    )
+    assert charge_connector_ids
