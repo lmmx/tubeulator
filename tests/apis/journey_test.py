6a05ca4b87843a1ffb292afd39dd238820f25fda
Louis Maddox <louismmx@gmail.com>
2024-05-20 18:13:03 +0100

    test: cover Journey API

--- tests/apis/journey_test.py
+++ tests/apis/journey_test.py
@@ -3,10 +3,20 @@
 from journey_data import all_endpoints
 from tubeulator import fetch
 
-tested_eps = set()
-untested_eps = {"forward_requests", "journey_results", "modes"}
+tested_eps = {"journey_results", "modes"}
+untested_eps = {"forward_requests"}
 
 
 def test_journey_endpoints():
     assert list(vars(fetch.journey)) == all_endpoints
     assert set(vars(fetch.journey)) == tested_eps.union(untested_eps)
+
+def test_journey_results():
+    journey_results = fetch.journey.journey_results(**{"from":"Tooting Bec Underground", "to":"Tooting Broadway Underground"})
+    assert journey_results
+
+
+def test_modes():
+    modes = fetch.journey.modes()
+    assert modes
+
