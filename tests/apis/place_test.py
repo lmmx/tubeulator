918ece621ad89cf71c0a65d16ee8fd93aa118d38
Louis Maddox <louismmx@gmail.com>
2024-05-20 18:41:06 +0100

    test: cover most of the Place API

--- tests/apis/place_test.py
+++ tests/apis/place_test.py
@@ -2,16 +2,18 @@
 
 from place_data import all_endpoints
 from tubeulator import fetch
+from pytest import mark
 
-tested_eps = set()
-untested_eps = {
+tested_eps = {
     "places_by_geo_region",
-    "forward_requests",
     "meta_categories",
-    "meta_place_types",
     "search_places",
     "places_by_type",
     "place_by_id",
+}
+untested_eps = {
+    "forward_requests",
+    "meta_place_types",
     "place_by_type_at_coordinates",
 }
 
@@ -19,3 +21,35 @@ untested_eps = {
 def test_place_endpoints():
     assert list(vars(fetch.place)) == all_endpoints
     assert set(vars(fetch.place)) == tested_eps.union(untested_eps)
+
+def test_places_by_geo_region():
+    places_by_geo_region = fetch.place.places_by_geo_region()
+    assert places_by_geo_region
+
+def test_meta_categories():
+    meta_categories = fetch.place.meta_categories()
+    assert meta_categories
+
+@mark.skip(reason="Receives list of strings but expects objects")
+def test_meta_place_types():
+    meta_place_types = fetch.place.meta_place_types()
+    assert meta_place_types
+
+def test_search_places():
+    search_places = fetch.place.search_places()
+    assert search_places
+
+
+def test_places_by_type():
+    places_by_type = fetch.place.places_by_type(types="CarPark")
+    assert places_by_type
+
+def test_place_by_id():
+    place_by_id = fetch.place.place_by_id()
+    assert place_by_id
+
+@mark.skip(reason="Value is returned empty, despite coming from places_by_type result.")
+def test_place_by_type_at_coordinates():
+    """Car parks near Morden Station."""
+    place = fetch.place.place_by_type_at_coordinates(type="CarPark", lat="51.404413", lon="-0.194517")
+    assert place
