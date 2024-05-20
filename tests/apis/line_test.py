d71db49759b6014a2eefea4aec0e8a5edf6d7c43
Louis Maddox <louismmx@gmail.com>
2024-05-20 01:58:06 +0100

    test: Initial coverage for Line endpoints

--- tests/apis/line_test.py
+++ tests/apis/line_test.py
@@ -1,22 +1,21 @@
 """Tests for the line API."""
 
+from datetime import datetime, timedelta
+
 from line_data import all_endpoints, snapshot_album
 from pytest import mark
 from tubeulator import fetch
 
 tested_eps = {
-    "lines_by_ids",
-    "lines_by_modes",
-    "meta_modes",
-}
-untested_eps = {
     "all_routes",
     "arrivals_by_ids",
     "arrivals_by_ids_stop",
     "disruption_by_ids",
     "disruption_by_modes",
-    "forward_requests",
+    "lines_by_ids",
+    "lines_by_modes",
     "meta_disruption_categories",
+    "meta_modes",
     "meta_service_types",
     "meta_severity",
     "route_by_ids",
@@ -31,6 +30,104 @@ untested_eps = {
     "timetable_by_id_from_stop",
     "timetable_by_id_from_to_stop",
 }
+untested_eps = {
+    "forward_requests",
+}
+
+
+def test_arrivals_by_ids():
+    arrivals_by_ids = fetch.line.arrivals_by_ids(ids="waterloo-city")
+
+
+def test_all_routes():
+    all_routes = fetch.line.all_routes()
+
+
+def test_arrivals_by_ids_stop():
+    arrivals_by_ids_stop = fetch.line.arrivals_by_ids_stop(
+        ids="waterloo-city", stopPointId="940GZZLUASL"
+    )
+
+
+def test_disruption_by_ids():
+    disruption_by_ids = fetch.line.disruption_by_ids(ids="waterloo-city")
+
+
+def test_disruption_by_modes():
+    disruption_by_modes = fetch.line.disruption_by_modes(modes="tube")
+
+
+@mark.skip()
+def test_forward_requests():
+    forward_requests = fetch.line.forward_requests()
+
+
+def test_meta_disruption_categories():
+    meta_disruption_categories = fetch.line.meta_disruption_categories()
+
+
+def test_meta_service_types():
+    meta_service_types = fetch.line.meta_service_types()
+
+
+def test_meta_severity():
+    meta_severity = fetch.line.meta_severity()
+
+
+def test_route_by_ids():
+    route_by_ids = fetch.line.route_by_ids(ids="waterloo-city")
+
+
+def test_route_by_modes():
+    route_by_modes = fetch.line.route_by_modes(modes="tube")
+
+
+def test_route_sequence_by_id_direction():
+    route_sequence_by_id_direction = fetch.line.route_sequence_by_id_direction(
+        id="waterloo-city", direction="outbound"
+    )
+
+
+def test_search_lines_routes():
+    search_lines_routes = fetch.line.search_lines_routes(query="Downing Street")
+
+
+def test_status_by_ids():
+    status_by_ids = fetch.line.status_by_ids(ids="waterloo-city")
+
+
+def test_status_by_ids_dates():
+    now = datetime.now()
+    start = (now + timedelta(days=6)).strftime("%Y-%m-%dT%H:%M:%S-00:00")
+    end = (now + timedelta(days=7)).strftime("%Y-%m-%dT%H:%M:%S-00:00")
+    status_by_ids_dates = fetch.line.status_by_ids_dates(
+        ids="waterloo-city", startDate=start, endDate=end
+    )
+
+
+def test_status_by_modes():
+    status_by_modes = fetch.line.status_by_modes(modes="tube")
+
+
+def test_status_by_severity():
+    status_by_severity = fetch.line.status_by_severity(severity=0)
+
+
+def test_stop_points_by_id():
+    stop_points_by_id = fetch.line.stop_points_by_id(id="waterloo-city")
+
+
+def test_timetable_by_id_from_stop():
+    timetable_by_id_from_stop = fetch.line.timetable_by_id_from_stop(
+        id="piccadilly", fromStopPointId="940GZZLUASL"
+    )
+
+
+def test_timetable_by_id_from_to_stop():
+    """Arsenal to Piccadilly Circus, via Piccadilly line."""
+    timetable_by_id_from_to_stop = fetch.line.timetable_by_id_from_to_stop(
+        id="piccadilly", fromStopPointId="940GZZLUASL", toStopPointId="940GZZLUPCC"
+    )
 
 
 def test_line_endpoints():
